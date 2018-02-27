dc0ee49d3990f5ad09d6b879b4228e7b313a03a8
Ed Perkins <ed@edperkins.co.uk>
2018-02-27 12:38:19 +0000

    fixed and organised tests

---
+++ ruby/spec/item_spec.rb
@@ -0,0 +1,11 @@
+require './lib/gilded_rose'
+
+describe Item do
+
+  def test_foo
+    items = [Item.new("foo", 0, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].name, "foo"
+  end
+
+end
