5724502f817bb2ce0b6721b04ef59bf637578bfe
Ed Perkins <ed@edperkins.co.uk>
2018-02-28 18:24:52 +0000

    update gilded rose and text tests

--- texttest_fixture.rb
+++ texttest_fixture.rb
@@ -1,15 +1,19 @@
 require './lib/gilded_rose'
 require './lib/item'
+require './lib/basic_item'
+require './lib/legendary_item'
+require './lib/aged_brie'
+require './lib/backstage_pass'
 
 items = [
-  Item.new(name = '+5 Dexterity Vest', sell_in = 10, quality = 20),
-  Item.new(name = 'Aged Brie', sell_in = 2, quality = 0),
-  Item.new(name = 'Elixir of the Mongoose', sell_in = 5, quality = 7),
-  Item.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = 0, quality = 80),
-  Item.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = -1, quality = 80),
-  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 15, quality = 20),
-  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 10, quality = 49),
-  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 5, quality = 49),
+  BasicItem.new(name = '+5 Dexterity Vest', sell_in = 10, quality = 20),
+  AgedBrie.new(name = 'Aged Brie', sell_in = 2, quality = 0),
+  BasicItem.new(name = 'Elixir of the Mongoose', sell_in = 5, quality = 7),
+  LegendaryItem.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = 0, quality = 80),
+  LegendaryItem.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = -1, quality = 80),
+  BackstagePass.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 15, quality = 20),
+  BackstagePass.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 10, quality = 49),
+  BackstagePass.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 5, quality = 49),
   # This Conjured item does not work properly yet
   Item.new(name = 'Conjured Mana Cake', sell_in = 3, quality = 6), # <-- :O
 ]
